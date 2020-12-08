bb0a9b837486fe2b4e4a6fc0058b78a206bf1597
Paul <pgregotski@gmail.com>
2020-12-08 10:06:04 -0500

    set util char read attempt

--- set.lua
+++ set.lua
@@ -2,8 +2,6 @@
     write config file of type of directive and run startup
 ]]--
 
-local sys = require("sys")
-
 local directives = { "miner", "dungeon" }
 
 print("Which Type of bot to install")
@@ -11,15 +9,20 @@ for key, value in pairs(directives) do
     print(tostring(key) .. ") " .. value)
 end
 print("Press a #")
+
 while true do
     local _, char = os.pullEvent("char")
-    break
+    if directives[tonumber(char)] == "n" then
+        break
+    else
+        error()
+    end
 end
 
 local index = tonumber(char)
 
-local directive = directives[char]
-sys.log("Installing directive " .. directive)
+local directive = directives[index]
+print("Installing directive " .. directive)
 local configFile = fs.open("/config", "w")
 configFile.writeLine(directive)
 configFile.close()
